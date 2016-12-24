a01a682794b82aa559bb10c56227959609252d0c
xudandan123 <905976615@qq.com>
2016-12-24 12:04:10 +0800

    保存

---
+++ js/page/home.js
@@ -0,0 +1,19 @@
+!function () {
+    $(function () {
+        $(".searchNameInput").focus(function () {
+            $(this).css("borderColor","#c40000");
+        }).blur(function () {
+            $(this).css("borderColor","#c2cad8");
+        });
+
+
+
+
+
+
+
+
+
+
+    });
+}();
